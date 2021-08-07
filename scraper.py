58644aa065c77599ea23d10959a44a7fc7343b59
jhinglish <jhinglish@gmail.com>
2021-08-07 21:48:42 +0200

    partial extract of url

--- scraper.py
+++ scraper.py
@@ -9,35 +9,41 @@ import re
 url = 'https://utopiaclivias.co.za/products/category/mature_plants?page=1'
 # page = urlopen(url)
 html = urlopen(url).read().decode("utf-8")
-
-name_pattern = r'<div class="field field--name-title field--type-string field--label-hidden field--item">.*?</div>'
-price_pattern = r'<div class="product--variation-field--variation_price__670 field field--name-price field--type-commerce-price field--label-hidden field--item">.*?</div>'
-
-browser = ms.Browser()
-x = (browser.get(url).soup.select('article')[1]).decode(eventual_encoding = "utf-8")
-print(x)
-y = re.search(name_pattern, x)
-print(y)
-y_sub = re.sub("<.*?>", "", y.group())
-y_sub
-
-n = 0
-while n < 11:
-    article = (browser.get(url).soup.select('article')[n]).decode(eventual_encoding = "utf-8")
-    results = re.search(name_pattern, article)
-    clivia = re.sub("<.*?>", "", results.group())
-    print(clivia)
-    n += 1
-    
-
-match_results = re.search(name_pattern, html)
-name = re.sub("<.*?>", "", match_results.group())
-print(name)
+# print(html)
 
 
 # read specific information
 # name/price/url
 
+name_pattern = r'<div class="field field--name-title field--type-string field--label-hidden field--item">.*?</div>'
+price_pattern = r'<div class="product--variation-field--variation_price.*?</div>'
+
+browser = ms.Browser()
+x = (browser.get(url).soup.select('article')[1]).decode(eventual_encoding = "utf-8")
+print(x)
+y = re.search(price_pattern, x)
+print(y)
+y_sub = re.sub("<.*?>", "", y.group())
+print(y_sub)
+z = urlopen(url)
+# links = [a['href'] for a in re.findall('a', x)]
+link_text = ''
+for link in re.findall('a', x):
+    link_text = link['href']
+    print(link_text)
+
+
+count = len(re.findall('<article', html))
+n = 0
+while n < count:
+    article = (browser.get(url).soup.select('article')[n]).decode(eventual_encoding = "utf-8")
+    name_results = re.search(name_pattern, article)
+    names = re.sub("<.*?>", "", name_results.group())
+    price_results = re.search(price_pattern, article)
+    prices = re.sub("<.*?>", "", price_results.group())
+    print(names + ' : ' + prices)
+    n += 1
+    
 
 # create/update list and save to file system
 # print out new plants
